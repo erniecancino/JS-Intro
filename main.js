94931f66b00c0e5aaf748f1facaebd11a5ca1d57
Ernie Caancino <erniecancino@gmail.com>
2016-04-20 13:31:39 -0500

    sending homwork for intro

---
+++ main.js
@@ -0,0 +1,77 @@
+/*=========================================================
+    Part 1: Variables
+*/
+var givenName;
+// Q: What does `givenName` equal right now?
+// A: givenName has no variable
+
+
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+// A: Tim is a string assigned to a variable
+
+
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+// A: givenName is reassigned to JD
+
+
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+// A: set variable with a string "Hello, how are you?"
+
+
+/*=========================================================
+     Part 2: Simple Math
+*/
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: math is set undifined da
+
+math = high - "5";
+// Q:  What is `math` set to?
+// A:  math is set to data high and object 5
+
+
+/* =========================================================
+     Part 3: Expressions
+*/
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 32 years old"
+// The answer shoud not be written in a comment.
+var born = 1983;
+var today = 2015;
+
+// Answers Below:
+console.log(givenName + {today -born}) "years old"
+
+
+// Store some information following in variables.
+var yourName = ;
+var instructorName;
+/
+var yourName =  {
+  name: "Ernie",
+  address: 32048,
+  city: "San Antonio"
+}
+
+ var instructorName = {
+   name: "Jason",
+   address: 45065,
+   city: "Universal City"
+}
+///
+// Update the variables above so the expression reads correctly.
+// Answers Below:
+
+
+// Final Statement
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
